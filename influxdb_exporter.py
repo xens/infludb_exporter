fd2547f6e7771d3e6eaa44364ee441ac086f8834
Romain <r.aviolat@gmail.com>
2017-05-11 00:53:04 +0200

    Dump all the measurements and add arg support

--- influxdb_exporter.py
+++ influxdb_exporter.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
 import datetime as dt
 import requests
 
-def get_metrics():
+def get_metrics(host, db, measurement):
     """
     Get metrics from the InfluxDB server's API
     """
-    url = "http://server:8086/query?pretty=true"
-    payload = {'db': 'database',
+    url = "%s/query?pretty=true" % (host)
+    payload = {'db': db,
                'chunk_size': '20000',
-               'q': 'SELECT * FROM \"metric\"'}
+               'q': 'SELECT * FROM {0}'.format(measurement)}
     r = requests.get(url, params=payload)
     data = json.loads(r.text)
     return data
 
+def get_measurements(host):
+    """
+    Get list of all the different measurements inside a
+    database.
+    """
+    url = "%s/query?pretty=true" % (host)
+    payload = {'db': 'smartmetter_db',
+               'chunk_size': '20000',
+               'q': 'SHOW MEASUREMENTS'}
+    r = requests.get(url, params=payload)
+    measurements = sum(json.loads(r.text)["results"][0]["series"][0]["values"], [])
+    return measurements
 
 def time_format(rfc3339):
     """
@@ -25,14 +38,14 @@ def time_format(rfc3339):
     """
     rfc3339=rfc3339[:-4] # stripping micro-second informations
     try:
-      epoch = dt.datetime.strptime(rfc3339, '%Y-%m-%dT%H:%M:%S.%f').timestamp()
-      return epoch
+        epoch = dt.datetime.strptime(rfc3339, '%Y-%m-%dT%H:%M:%S.%f').timestamp()
+        return epoch
     except ValueError:
-      print("Timestamp: %s is wrongly formated" % (rfc3339))
-      return 1
+        #print("Timestamp: %s is wrongly formated" % (rfc3339))
+        return 1
 
 
-def json_parser(data):
+def json_parser(data, measurement):
     """
     Extract the relevant information inside json values fields
     """
@@ -40,20 +53,39 @@ def json_parser(data):
     count_failed=0
 
     for i in range(len(data)):
-      epoch=time_format(data[i][0])
-      if epoch != 1:
-        host=data[i][1]
-        value=data[i][2]
-        count=count+1
-        file_out.write("%s,host=%s value=%s %s\n" % (db_name, host, value, int(epoch)))
-      else:
-        count_failed=count_failed+1
-        continue
+        epoch=time_format(data[i][0])
+        if epoch != 1:
+          host=data[i][1]
+          value=data[i][2]
+          count=count+1
+          file_out.write("%s,host=%s value=%s %s\n" % (measurement, host, value, int(epoch)))
+        else:
+          count_failed=count_failed+1
+          continue
 
-    file_out.close()
-    print("entries parsed: %s, failed: %s" % (count, count_failed))
+    print("measurement: %s, entries parsed: %s, failed: %s" % (measurement, count, count_failed))
 
-raw_data = get_metrics()
-db_name = raw_data["results"][0]["series"][0]["name"]
-file_out = open('output_lineformat', 'w')
-json_parser(raw_data["results"][0]["series"][0]["values"])
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', required=True, help="hosts to connect to ex: http://192.168.1.1:8086")
+    parser.add_argument('--db', required=True, help="name of the DB to dump")
+    parser.add_argument('--file', required=True, help="name of the file to write the data to")
+    args = parser.parse_args()
+
+    if args.host and args.db and args.file:
+        measurements = get_measurements(args.host)
+        file_out = open(args.file, 'w')
+        raw_data={}
+        for i in measurements:
+            temp_data=(get_metrics(args.host, args.db, i))
+            raw_data.update({i: temp_data})
+
+        for key in raw_data:
+          json_parser(raw_data[key]["results"][0]["series"][0]["values"], key)
+        file_out.close()
+    else:
+        print("missing option, try -h (help)")
+
+
